dbd22f8518e2f5c56c5b4f585c159f0046f5375a
axell787 <simca.skokanova@gmail.com>
2026-04-16 09:29:52 +0200

    comments on Assignment 02

--- Assignment02.R
+++ Assignment02.R
@@ -3,10 +3,24 @@ library(reshape2)
 library(ggplot2)
 
 runoff_stations <- readRDS("../data/runoff_stations.rds")
-subset <- runoff_stations[, .(sname, area, altitude)]
 
+### PART 1 ###
+
+#subset from runoff_stations
+subset <- runoff_stations[, .(sname, area, altitude)]
+#transformed to tidy format
 tidy <- reshape2::melt(subset, id.vars = "sname")
 
+
+### PART 2 ###
+
+# Create a geom_point plot of area (x axis) vs. altitude (y axis).
+ggplot(runoff_stations, aes(x = area, y = altitude))+
+  geom_point()
+
+### PART 3 ###
+
+#reproduced graphs
 ggplot(runoff_stations, aes(x = area, y = altitude, color = area,)) +
   geom_point() +
   geom_text(aes(label = sname), vjust = -1) +
@@ -21,6 +35,9 @@ ggplot(runoff_stations, aes(x = lon, y = lat, color = altitude)) +
   theme_bw() +
   labs(x = "lon", y = "lat")
 
+### PART 4 ###
+
+#graph comparing the periods of available data
 ggplot(runoff_stations, aes(y = sname)) +
   geom_linerange(aes(xmin = start, xmax = end), 
                  color = "steelblue", size = 2) +
